30665cbb558d004ebb2447f2ed2a4c116ced9785
Kate Aplin <kaplin@hmc.edu>
2014-09-10 19:37:17 -0700

    Created a grid view, fixed to be an iPad app

---
+++ viewTutorial/KAGrid.m
@@ -0,0 +1,31 @@
+//
+//  KAGrid.m
+//  viewTutorial
+//
+//  Created by Kathryn Aplin on 9/10/14.
+//  Copyright (c) 2014 Kathryn Aplin. All rights reserved.
+//
+
+#import "KAGrid.h"
+
+@implementation KAGrid
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
--- viewTutorial/KAViewController.m
+++ viewTutorial/KAViewController.m
@@ -6,9 +6,12 @@
 //  Copyright (c) 2014 Kathryn Aplin. All rights reserved.
 //
 
+#import "KAGrid.h"
 #import "KAViewController.h"
 
-@interface KAViewController ()
+@interface KAViewController () {
+    UIView *_gridView;
+}
 
 @end
 
@@ -18,6 +21,21 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    // Create grid frame.
+    CGRect frame  = self.view.frame;
+    CGFloat x = CGRectGetWidth(frame)*.1;
+    CGFloat y = CGRectGetHeight(frame)*.1;
+    CGFloat size = MIN(CGRectGetWidth(frame), CGRectGetHeight(frame))*.80;
+    
+    CGRect gridFrame = CGRectMake(x, y, size, size);
+    
+    // Create grid view.
+    _gridView = [[KAGrid alloc] initWithFrame:gridFrame];
+    _gridView.backgroundColor = [UIColor blackColor];
+    [self.view addSubview:_gridView];
 }
 
 - (void)didReceiveMemoryWarning
