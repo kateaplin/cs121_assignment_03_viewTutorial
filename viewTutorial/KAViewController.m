e14ba7d71b6b0a57d6b5f38602b6881e9e0b7ac9
Kate Aplin <kaplin@hmc.edu>
2014-09-10 19:58:42 -0700

    Finished views tutorial. Button glows when pressed to show touch.

--- viewTutorial/KAViewController.m
+++ viewTutorial/KAViewController.m
@@ -10,6 +10,7 @@
 #import "KAViewController.h"
 
 @interface KAViewController () {
+    UIButton *_button;
     UIView *_gridView;
 }
 
@@ -36,12 +37,40 @@
     _gridView = [[KAGrid alloc] initWithFrame:gridFrame];
     _gridView.backgroundColor = [UIColor blackColor];
     [self.view addSubview:_gridView];
+    
+    // Create button.
+    CGFloat buttonSize = size/5.0;
+    CGRect buttonFrame = CGRectMake(0, 0, buttonSize, buttonSize);
+    _button = [[UIButton alloc] initWithFrame:buttonFrame];
+    _button.backgroundColor = [UIColor redColor];
+    
+    // Configure button text.
+    [_button setTitle:@"Hit me" forState:UIControlStateNormal];
+    [_button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    
+    // Makes the button 'glow' when touched, as a highlighting effect.
+    // We do not change the color since we are using a normal UI button,
+    // but not providing an image as the background. If there were an image,
+    // it would darken automatically. If we wanted to change the background
+    // color, we would need to override methods of the UIButton class.
+    _button.showsTouchWhenHighlighted = YES;
+    
+    [_gridView addSubview:_button];
+    
+    // Create target for button.
+    [_button addTarget:self action:@selector(buttonPressed:)
+      forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)buttonPressed:(id)sender
+{
+    NSLog(@"You pressed the button!");
 }
 
 @end
